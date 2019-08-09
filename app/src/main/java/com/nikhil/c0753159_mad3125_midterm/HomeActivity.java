3917ada0644ca74cf30303dd8e79774df3e3e64b
Nikhil <nikhilthakur050@gmail.com>
2019-08-09 05:50:25 -0400

    Recycler View Designing

--- app/src/main/java/com/nikhil/c0753159_mad3125_midterm/HomeActivity.java
+++ app/src/main/java/com/nikhil/c0753159_mad3125_midterm/HomeActivity.java
@@ -21,33 +21,62 @@ import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 
-public class HomeActivity extends AppCompatActivity {
-
- Context context;
+public class HomeActivity extends AppCompatActivity implements MyRecycler{
     public ArrayList<SatData> mSatData ;
 
-    public ArrayList<SatData> getmSatData() {
-        return mSatData;
-    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        processJSON();
-        for (int i=0; i<mSatData.size();i++)
+
+
+        String userJson = null;
+        try {
+            userJson = loadJSONFromAsset();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        if(userJson != null)
         {
-            mSatData.get(i).display();
+
+            try
+            {
+                JSONArray mJSONArray = new JSONArray(userJson);
+                for (int i = 0; i < mJSONArray.length(); i++)
+                {
+                    JSONObject mObject = mJSONArray.getJSONObject(i);
+                    System.out.println("----------------");
+                   // System.out.println(mObject.get("mission_name"));
+                    //Log.d("DATA", mObject.toString());
+                    SatData mSatdata = new SatData();
+                    mSatdata.setFlight_number(mObject.get("flight_number").toString());
+                    mSatdata.setMission_name(mObject.get("mission_name").toString());
+                    mSatdata.setLaunch_year(mObject.get("launch_year").toString());
+                    mSatdata.setUpcoming(mObject.get("upcoming").toString());
+                    mSatdata.display();
+
+
+
+
+                }
+            }
+            catch (JSONException e)
+            {
+                e.printStackTrace();
+            }
         }
     }
-    public String loadJSONFromAsset() {
+
+
+    public String loadJSONFromAsset() throws JSONException {
         String json;
         try {
-            InputStream is = context.getAssets().open("jsonfile.json");
+            InputStream is = getAssets().open("jsonfile.json");
             int size = is.available();
             byte[] buffer = new byte[size];
-            int count = is.read(buffer);
+            is.read(buffer);
             is.close();
-            //Log.d("-- COUNT --", String.format("%d Bytes",count));
             json = new String(buffer, StandardCharsets.UTF_8);
         } catch (IOException ex) {
             ex.printStackTrace();
@@ -56,113 +85,13 @@ public class HomeActivity extends AppCompatActivity {
         return json;
     }
 
-    public void processJSON()
-    {
-        String jsonString = this.loadJSONFromAsset();
-        if (jsonString != null)
-        {
-            try {
-                JSONArray mJSONArray = new JSONArray(jsonString);
-                mSatData = new ArrayList<SatData>();
 
-                for (int i = 0; i < mJSONArray.length(); i++) {
-
-                    SatData mSatdata = getFlightDetailObjectFromJSON(mJSONArray.getJSONObject(i));
-                    mSatData.add(mSatdata);
-                    //Log.d("-- JSON -- ", mSatData.toString());
-
-                }
-            }
-            catch(JSONException e)
-            {
-                e.printStackTrace();
-            }
-        }
-    }
-
-    public SatData getFlightDetailObjectFromJSON(JSONObject userJsonObject) throws JSONException
-    {
-        String flight_number = userJsonObject.getString("flight_number");
-        String mission_name = userJsonObject.getString("mission_name");
-        String upcoming = userJsonObject.getString("upcoming");
-        String launch_year = userJsonObject.getString("launch_year");
-        String launch_window = userJsonObject.getString("launch_window");
-        String details = userJsonObject.getString("details");
-
-        //Read Rocket
-        JSONObject rocket = new JSONObject(userJsonObject.getJSONObject("rocket").toString());
-        String rocket_id = rocket.getString("rocket_id");
-        String rocket_name = rocket.getString("rocket_name");
-        String rocket_type = rocket.getString("rocket_type");
-
-        //Read Launch Site
-        JSONObject launchSite = new JSONObject(userJsonObject.getJSONObject("launch_site").toString());
-        String site_id = launchSite.getString("site_id");
-        String site_name = launchSite.getString("site_name");
-        String site_name_long = launchSite.getString("site_name_long");
-
-        //Read Links
-        JSONObject links = new JSONObject(userJsonObject.getJSONObject("links").toString());
-        String mission_patch = links.getString("mission_patch");
-        String mission_patch_small = links.getString("mission_patch_small");
-        String article_link = links.getString("article_link");
-        String wikipedia = links.getString("wikipedia");
-        String video_link = links.getString("video_link");
-
-        //start creating User object
-        SatData mSatdata = new SatData();
-        mSatdata.setFlight_number(flight_number);
-        mSatdata.setMission_name(mission_name);
-        mSatdata.setUpcoming(upcoming);
-        mSatdata.setLaunch_year(launch_year);
-        mSatdata.setLaunch_window(launch_window);
-        mSatdata.setDetails(details);
-
-        Rdata mRocket = new Rdata();
-        mRocket.setRocket_id(rocket_id);
-        mRocket.setRocket_name(rocket_name);
-        mRocket.setRocket_type(rocket_type);
-        mSatdata.setRocket(mRocket);
-
-        Launch mLaunchSite = new Launch();
-        mLaunchSite.setSite_id(site_id);
-        mLaunchSite.setSite_name(site_name);
-        mLaunchSite.setSite_name_long(site_name_long);
-        mSatdata.setLaunchSite(mLaunchSite);
-
-        Weblinks mLinks = new Weblinks();
-        mLinks.setMission_patch(mission_patch);
-        mLinks.setMission_patch_small(mission_patch_small);
-        mLinks.setArticle_link(article_link);
-        mLinks.setWikipedia(wikipedia);
-        mLinks.setVideo_link(video_link);
-        //mSatData.setLinks(mLinks);
-
-        return mSatdata;
-    }
-
-}
-
-       /* MyListData[] myListData = new MyListData[]{
-                new MyListData("Email", android.R.drawable.ic_dialog_email),
-                new MyListData("Info", android.R.drawable.ic_dialog_info),
-                new MyListData("Delete", android.R.drawable.ic_delete),
-                new MyListData("Dialer", android.R.drawable.ic_dialog_dialer),
-                new MyListData("Alert", android.R.drawable.ic_dialog_alert),
-                new MyListData("Map", android.R.drawable.ic_dialog_map),
-                new MyListData("Email", android.R.drawable.ic_dialog_email),
-                new MyListData("Info", android.R.drawable.ic_dialog_info),
-                new MyListData("Delete", android.R.drawable.ic_delete),
-                new MyListData("Dialer", android.R.drawable.ic_dialog_dialer),
-                new MyListData("Alert", android.R.drawable.ic_dialog_alert),
-                new MyListData("Map", android.R.drawable.ic_dialog_map),
-        };
 
         RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
-        MyListAdapter adapter = new MyListAdapter(myListData);
+        SatData adapter = new MyListAdapter();
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(adapter);
     }
-}*/
+
 
